f377797e4c5c30d0e5b099d7cfe79222a5720c0f
Dhiraj <ddhole47@gmail.com>
2025-07-05 17:15:40 +0530

    initial commit

---
+++ src/main/java/com/online_admission_process/creation/Entites/Enquiry.java
@@ -0,0 +1,84 @@
+package com.online_admission_process.creation.Entites;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Enquiry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int enquiry_id;
+
+    private int sid;
+    private String enquiry_message;
+    private String enquiry_date;
+    private String enquiry_remark;
+
+    public Enquiry() {
+        super();
+    }
+
+    public Enquiry(int enquiry_id, int sid, String enquiry_message, String enquiryDate, String enquiry_remark) {
+        this.enquiry_id = enquiry_id;
+        this.sid = sid;
+        this.enquiry_message = enquiry_message;
+        enquiry_date = enquiryDate;
+        this.enquiry_remark = enquiry_remark;
+    }
+
+    public int getEnquiry_id() {
+
+        return enquiry_id;
+    }
+
+    public void setEnquiry_id(int enquiry_id) {
+        this.enquiry_id = enquiry_id;
+    }
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public String getEnquiry_message() {
+        return enquiry_message;
+    }
+
+    public void setEnquiry_message(String enquiry_message) {
+        this.enquiry_message = enquiry_message;
+    }
+
+    public String getEnquiry_remark() {
+        return enquiry_remark;
+    }
+
+    public void setEnquiry_remark(String enquiry_remark) {
+        this.enquiry_remark = enquiry_remark;
+    }
+
+    public String getEnquiry_date() {
+        return enquiry_date;
+    }
+
+    public void setEnquiry_date(String enquiry_date) {
+        this.enquiry_date = enquiry_date;
+    }
+
+    @Override
+    public String toString() {
+        return "Enquiry{" +
+                "enquiry_id=" + enquiry_id +
+                ", sid=" + sid +
+                ", enquiry_message='" + enquiry_message + '\'' +
+                ", enquiry_date='" + enquiry_date + '\'' +
+                ", enquiry_remark='" + enquiry_remark + '\'' +
+                '}';
+    }
+}
